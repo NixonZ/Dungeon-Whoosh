e3b1a5bc8b2a124dbbf4fc2d3307c5031a39707a
Nalin Shani <nalinshani14@gmail.com>
2016-11-07 14:46:31 +0530

    Add files via upload

---
+++ Fundamentals/Player-Attributes-and-stuff/Attributes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+/*(i)Strength(S)
+    (ii)Defense(D)
+    (iii)Knowledge(IQ)
+    (iv)Agility(A)
+    (v)Courage(C)*/
+
+void attributes()
+{
+    int S,D,IQ,A,C;
+}
