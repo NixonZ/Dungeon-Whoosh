4ddceb6e7d232bc0f1923bd9ffa052f3b64ed5aa
Nalin Shani <nalinshani14@gmail.com>
2016-11-07 14:44:06 +0530

    Add files via upload

---
+++ Fundamentals/Player-Attributes-and-stuff/Player type.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void playertype()
+{
+    /*(i)Strength(S)
+    (ii)Defense(D)
+    (iii)Knowledge(IQ)
+    (iv)Agility(A)
+    (v)Courage(C)*/
+    char *playertype;
+    if (max(S,D,IQ,A,C)==S&&min(S,D,IQ,A,C)==A)
+    playertype={"Knight"};
+    else if (max(S,D,IQ,A,C)==A&&min(S,D,IQ,A,C)==S)
+    playertype={"Archer"};
+    else if (max(S,D,IQ,A,C)==IQ&&min(S,D,IQ,A,C)==D)
+    playertype={"Mage"};
+    else
+    playertype={"Warrior"};
+}
