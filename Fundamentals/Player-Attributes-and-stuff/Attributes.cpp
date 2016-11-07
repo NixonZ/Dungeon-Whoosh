a852a4e284145526d37f71a2c357c874b325c36a
Nalin Shani <nalinshani14@gmail.com>
2016-11-07 16:03:28 +0530

    Update Attributes.cpp

--- Fundamentals/Player-Attributes-and-stuff/Attributes.cpp
+++ Fundamentals/Player-Attributes-and-stuff/Attributes.cpp
@@ -8,5 +8,19 @@ using namespace std;
 
 void attributes()
 {
-    int S,D,IQ,A,C;
+    int S[7],D[7],IQ[7],A[7],C[7];
+    /* player-0
+       wolf-1
+       ogre-2
+       warrior(enemy)-3
+       knight(enemy)-4
+       Paladin(enemy)-5
+       Dragon-6*/
+       S[0]=,D[0]=,IQ[0]=,A[0]=,C[0]=;
+       S[1]=,D[1]=,IQ[1]=,A[1]=,C[1]=;
+       S[2]=,D[2]=,IQ[2]=,A[2]=,C[2]=;
+       S[3]=,D[3]=,IQ[3]=,A[3]=,C[3]=;
+       S[4]=,D[4]=,IQ[4]=,A[4]=,C[4]=;
+       S[5]=,D[5]=,IQ[5]=,A[5]=,C[5]=;
+       S[6]=,D[6]=,IQ[6]=,A[6]=,C[6]=;
 }
