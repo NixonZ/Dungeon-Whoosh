4924f36b72e229718cf561cf64e14bb6a75a1ab1
Nalin Shani <nalinshani14@gmail.com>
2016-11-07 17:45:40 +0530

    Add files via upload

---
+++ Fundamentals/Player-Attributes-and-stuff/health.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void heath()
+{
+    /* player-0
+       wolf-1
+       ogre-2
+       warrior(enemy)-3
+       knight(enemy)-4
+       Paladin(enemy)-5
+       Dragon-6*/
+    hp[7];
+    hp[0]=20*level*20;
+    hp[1]=20;
+    hp[2]=35;
+    hp[3]=40;
+    hp[4]=50;
+    hp[6]=75;
+    hp[7]=100;
+}
