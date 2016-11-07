860f02848e0176f0e312cf433b3707926bc2e36c
Nalin Shani <nalinshani14@gmail.com>
2016-11-07 18:15:51 +0530

    Add files via upload

---
+++ Fundamentals/Battle/Battle mechanincs.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+using namespace std;
+int battle(int a1)
+{
+    int damage;
+    damage=((S[0]+C[0])+(level+rand(3))*(hitp+magep))/(10*(defp+D[a1]-level))
+    return(damage);
+}
