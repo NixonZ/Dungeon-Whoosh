8eb55673a8e6c3bf6a6fc8d0fa32dcd4a1957fc6
Nalin Shani <nalinshani14@gmail.com>
2016-11-08 14:26:56 +0530

    Update Weapon attributes and defination.cpp

--- Fundamentals/Weapons/Weapon attributes and defination.cpp
+++ Fundamentals/Weapons/Weapon attributes and defination.cpp
@@ -6,32 +6,35 @@ void weaponsattributes()
     /*Attributes would be
       hit points
       defense points*/
-      int hitp,defp,magep; char *playerhand;
-if(stricmp(playerhand,"sword"))
+      int hitp,defp,magep=0,wear=20; char *playerhand;
+if(!(stricmp(playerhand,"sword")))
 {
     hitp=8+level*2;
     defp=level*2-6;
+
 }
-else if(stricmp(playerhand,"bow"))
+else if(!(stricmp(playerhand,"bow")))
 {
     if(stricmp(playertype,"archer")!=0)
-    hitp=10+level*2;
+    hitp=8+level*3;
     else
-        hitp=12+level*3;
+        hitp=6+level*2;
     defp=level*2-10;
 }
-else if(stricmp(playerhand,"staff"))
+else if(!(stricmp(playerhand,"staff")))
 {
-    if(stricmp(playertype,"mage")!=0)
-    hitp=3+level*2;
+    if(!(stricmp(playertype,"mage")))
+    {
+        hitp=3+level*2;
+        magep=level*3+2;
+    }
     else
     {
         hitp=3+level*3;
-        magep=level*3+2;
     }
     defp=level*2-4;
 }
-else if(stricmp(playerhand,"katana"))
+else if(!(stricmp(playerhand,"katana")))
 {
     if(stricmp(playertype,"warrior")!=0)
         hitp=10+level*3;
@@ -39,10 +42,48 @@ else if(stricmp(playerhand,"katana"))
         hitp=10+level*2;
     defp=level*3-3;
 }
-else if(stricmp(playerhand,"shield"))
+else if(!(stricmp(playerhand,"shield")))
 {
     hitp=0+level*2;
     defp=level*4+4;
+}
+else if(stricmp(playerhand,"greatsword"))
+{
+    if(stricmp(playertype,"knight")!=0)
+        hitp=20+level*3;
+    else
+        hitp=15+level*3;
+    defp=level*2-6;
+}
+else if(stricmp(playerhand,"crossbow"))
+{
+    if(stricmp(playertype,"archer")!=0)
+        hitp=13+level*3;
+        
+    else
+        hitp=10+level*2;
+    defp=level*3-10;
+}
+else if(!(stricmp(playerhand,"escutcheon")))
+{
+    hitp=0+level*3;
+    defp=level*5+5;
+}
+else if(!(stricmp(playerhand,"tachi")))
+{
+    if(stricmp(playertype,"warrior")!=0)
+        hitp=15+level*3;
+    else
+        hitp=12+level*2;
+    defp=level*3-1;
+}
+else if(stricmp(playerhand,"dragon-sword"))
+{
+    if(stricmp(playertype,"knight")!=0)
+        hitp=25+level*3;
+    else
+        hitp=20+level*4;
+    defp=level*3-6;
 }
 else
 {
