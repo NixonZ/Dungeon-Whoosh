d79d66364df8d82f4a18ede9052a6ba97302ee8f
GitHG22 <hg1229@gmail.com>
2016-11-08 15:05:18 +0530

    Hospital made

---
+++ Fundamentals/Player-Attributes-and-stuff/hospital.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+void hospital(int why){
+  if(why==0){
+    cout<<"\nWelcome to Whoosh Center!\nWe have restored your health to full.\nWe hope to see you again!"
+  }
+  else if(why==1){
+    cout<<"\nYou were defeated. Your health has been fully restored.\nBetter luck next time!"
+  }
+  healthy=h[0];
+}
