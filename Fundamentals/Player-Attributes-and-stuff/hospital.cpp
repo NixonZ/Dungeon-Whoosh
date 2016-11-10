30cad11ed5be54f94d68d15b02585d7b7ae59870
Nalin Shani <nalinshani14@gmail.com>
2016-11-10 23:39:27 +0530

    Update hospital.cpp

--- Fundamentals/Player-Attributes-and-stuff/hospital.cpp
+++ Fundamentals/Player-Attributes-and-stuff/hospital.cpp
@@ -11,4 +11,20 @@ void hospital(int why){
     cout<<"\nYou were defeated. Your health has been fully restored.\nBetter luck next time!";
   }
   healthy=hp[0];
+  cout<<"Would you like to buy a potion(T/F)\n";
+  char x2;
+  cin>>x2;
+  if(x2=='T'||x2=='t')
+  {
+      if(playerbag.potion_static<5)
+      {
+          cout<<"You have bought a potion\n";
+          money-=50;
+          playerbag.potion_static++;
+      }
+      else
+        cout<<"No more space\n";
+  }
+  else
+    cout<<"Bye-bye\n";
 }
