99ac3462e5622a4fad73368b73426dd21571b968
Nalin Shani <nalinshani14@gmail.com>
2016-11-10 23:41:09 +0530

    Update variabless.h

--- Headers/variabless.h
+++ Headers/variabless.h
@@ -1,6 +1,11 @@
 #ifndef VARIABLESS_H_INCLUDED
 #define VARIABLESS_H_INCLUDED
 
+struct bag
+{
+    int potion_static;
+    char weapon_static[3][20];
+};
 extern int S[7],D[7],IQ[7],A[7],C[7];
 extern int hitp,defp,magep,wear;
 extern char *playerhand;
@@ -8,6 +13,6 @@ extern int money;
 extern int expe,level;
 extern int hp[7],healthy;
 extern char *playertype;
-
+extern bag playerbag;
 
 #endif
