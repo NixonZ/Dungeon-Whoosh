e46abe268272ff9deb61c020d158ed25c0eb4a1e
Nalin Shani <nalinshani14@gmail.com>
2016-11-12 21:06:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,23 +12,19 @@ int hp[7],healthy;
 char *playertype="Bare hand";
 char *name;
 bag playerbag;
-/*struct bag
-{
-    int potion_static;
-    char weapon_static[3][20];
-};*/
 
 using namespace std;
 
 int main()
 {
-  strcpy(playerbag.weapon_static[0],"Bare hand");
-  strcpy(playerbag.weapon_static[1],"Bare hand");
-  strcpy(playerbag.weapon_static[2],"Bare hand");
+  playerbag.potion_static=0;
+  playerbag.weapon_static1="Bare hand";
+  playerbag.weapon_static2="Bare hand";
+  playerbag.weapon_static3="Bare hand";
   health();
   S[0]=4;D[0]=4;IQ[0]=4;A[0]=4;C[0]=4;
   cout<<"welcome adventurer\n";
-  cout<<"your name is name\n";
+  gets(name);
   name="name";
   ask();
   playertypo();
@@ -36,8 +32,9 @@ int main()
   puts(playertype);
   cout<<"You health is "<<hp[0]<<endl;
   cout<<"Strength="<<S[0]<<endl<<"Knowledge="<<IQ[0]<<endl<<"Agility="<<A[0]<<endl<<"Defense="<<D[0]<<endl<<"Courage="<<C[0];
-  cout<<"how much $$ u want?\n";
+  cout<<"\nhow much $$ u want?\n";
   cin>>money;
   armoury();
-  cout<<battle(1);
+  cout<<endl<<battle(1)<<endl;
+  bagf();
   }
