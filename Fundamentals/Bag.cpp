500794bf1e1fdad8be93eed237e312a58b5c627f
Nalin Shani <nalinshani14@gmail.com>
2016-11-12 21:10:10 +0530

    Update Bag.cpp

--- Fundamentals/Bag.cpp
+++ Fundamentals/Bag.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void bagf()
 {
     cout<<"\nWhat do you want to do\n1-Use potions\n2-Exchange weapons\ne-exit\n";
-    char x3; char *tempbg; int i;
+    char x3; char *tempbg; int i=1;
     do{
     cin>>x3;
     switch(x3)
@@ -27,20 +27,29 @@ void bagf()
     case '2':
         {
             cout<<"Which weapon do you want to select?\n";
-            cout<<"1-"; cout.write(playerbag.weapon_static[0],strlen(playerbag.weapon_static[0]));
-            cout<<"2-"; cout.write(playerbag.weapon_static[0],strlen(playerbag.weapon_static[1]));
-            cout<<"3-"; cout.write(playerbag.weapon_static[0],strlen(playerbag.weapon_static[2]));
+            cout<<"1-"; cout.write(playerbag.weapon_static1,strlen(playerbag.weapon_static1));
+            cout<<"2-"; cout.write(playerbag.weapon_static2,strlen(playerbag.weapon_static2));
+            cout<<"3-"; cout.write(playerbag.weapon_static3,strlen(playerbag.weapon_static3));
             cout<<endl;
-            do
+            while(i==1||i==2||i==3)
             {
                 cin>>i;
                 if(i>3)
                     break;
-                strcpy(tempbg,playerhand);
-                strcpy(playerhand,playerbag.weapon_static[i]);
-                strcpy(playerbag.weapon_static[i],tempbg);
+                if(i==1)
+                    tempbg=playerhand;
+                    playerhand=playerbag.weapon_static1;
+                    playerbag.weapon_static1=tempbg;
+                if(i==2)
+                    tempbg=playerhand;
+                    playerhand=playerbag.weapon_static2;
+                    playerbag.weapon_static2=tempbg;
+                if(i==3)
+                    tempbg=playerhand;
+                    playerhand=playerbag.weapon_static3;
+                    playerbag.weapon_static3=tempbg;
                 cout<<"Do you wish to continue?(enter 4 to exit)\n";
-            }while(i==1&&i==2&&i==3);
+            }
             break;
         }
     }
