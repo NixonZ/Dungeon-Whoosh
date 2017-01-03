e13b23354d82bd334dd142f847afe25b7d652790
NixonZ <nalinshani14@gmail.com>
2017-01-03 19:30:51 +0530

    I dont know

--- Fundamentals/Battle/Battle mechanics.cpp
+++ Fundamentals/Battle/Battle mechanics.cpp
@@ -3,17 +3,18 @@
 #include<stdlib.h>
 #include "../../Headers/variabless.h"
 #include "../../Headers/pointstuff.h"
+extern int S[7],D[7],IQ[7],A[7],C[7];
 using namespace std;
 float damage(int a1)
 {
-    float damage;
-    damage=((S[0]+C[0])+(level+1+rand()%3)*(hitp+magep))/(10*(defp+D[a1]-level));
+    float d1;
+    d1=((S[0]+C[0])+(level+1+rand()%3)*(hitp+magep))/(10*(defp+D[a1]-level));
     wear--;
     expe1();
-    return damage;
+    return d1;
 }
-battle()
+void battle()
 {
-
+    cout<<endl;
 }
 
--- Fundamentals/Player-Attributes-and-stuff/Attributes.cpp
+++ Fundamentals/Player-Attributes-and-stuff/Attributes.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include"../../Headers/pointstuff.h"
 #include "../../Headers/variabless.h"
 using namespace std;
 /*(i)Strength(S)
@@ -9,7 +10,6 @@ using namespace std;
 
 void attributes()
 {
-    extern int S[7],D[7],IQ[7],A[7],C[7];
     /* player-0
        wolf-1
        ogre-2
@@ -18,10 +18,10 @@ void attributes()
        Paladin(enemy)-5
        Dragon-6*/
        S[0]=4;D[0]=4;IQ[0]=4;A[0]=4;C[0]=4;
-       S[1]=5,D[1]=5,IQ[1]=5,A[1]=5,C[1]=5;
-       /*S[2]=,D[2]=,IQ[2]=,A[2]=,C[2]=10;
-       S[3]=,D[3]=,IQ[3]=,A[3]=,C[3]=;
-       S[4]=,D[4]=,IQ[4]=,A[4]=,C[4]=;
-       S[5]=,D[5]=,IQ[5]=,A[5]=,C[5]=;
-       S[6]=,D[6]=,IQ[6]=,A[6]=,C[6]=;*/
+       S[1]=5;D[1]=5;IQ[1]=5;A[1]=5;C[1]=5;
+       S[2]=6;D[2]=6;IQ[2]=6;A[2]=6;C[2]=6;
+       S[3]=7;D[3]=7;IQ[3]=7;A[3]=7;C[3]=7;
+       S[4]=8;D[4]=8;IQ[4]=8;A[4]=8;C[4]=8;
+       S[5]=9;D[5]=9;IQ[5]=9;A[5]=9;C[5]=9;
+       S[6]=10;D[6]=10;IQ[6]=10;A[6]=10;C[6]=10;
 }
